d698dfbe5e15b73abdbdf02c7fa7bff1a42b6874
Anastasiya Rassolova <avmurueva@gmail.com>
2020-09-23 19:51:13 +0300

    Revert "ci: break"
    This reverts commit c05c267f39c1b879e32d01114124f2f83b739f9e.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -21,7 +21,7 @@ fun main() {
     println("Commission: $commission5 kopecks")
 }
 
-fu calculateCommission(
+fun calculateCommission(
     cardType: String = "VK Pay",
     moneyAmount: Int,
     transfersSum: Int = 0
